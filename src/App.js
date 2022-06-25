2a8284d8f8b615be6a0d7063c3278a18d931e525
vistej <teja.pvt@gmail.com>
2022-06-25 10:11:24 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import AllWords from './components/AllWords';
 
 function App() {
   return (
     <div>
-      <AllWords />
+      <Routes>
+        <Route path="/" element={<AllWords />} >
+          <Route path=":group" element={<AllWords />} >
+            <Route path=":word" element={<AllWords />} />
+            </Route>
+        </Route>
+        <Route path="*" element={<Navigate to='/' replace />} />
+      </Routes>
     </div>
   );
 }
--- src/components/WordList.js
+++ src/components/WordList.js
@@ -1,27 +1,32 @@
 import axios from 'axios';
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 const WordList = () => {
   const [words, setWords] = React.useState();
   const [selectedGroup, setSelectedGroup] = React.useState(null);
   const [selectedWord, setSelectedWord] = React.useState(null);
   const [meaning, setMeaning] = React.useState(null);
+  const { group, word } = useParams();
 
   React.useEffect(() => {
-    axios.get('./data/words.json').then((res) => {
+    if (group) {
+      setSelectedGroup(group);
+      setSelectedWord(null);
+      setMeaning(null);
+    }
+    if (word) {
+      setSelectedWord(word);
+      setMeaning(null);
+    }
+  }, [group, word]);
+
+  React.useEffect(() => {
+    const url = `${window.location.origin}/vocab/data/words.json`;
+    axios.get(url).then((res) => {
       setWords(res.data);
     });
   }, []);
-
-  // React.useEffect(() => {
-  //   if (selectedWord) {
-  //     axios
-  //       .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${selectedWord}`)
-  //       .then((res) => {
-  //         setMeaning(res.data[0]);
-  //       });
-  //   }
-  // }, [selectedWord]);
 
   const showMeaning = () => {
     if (selectedWord && (!meaning || meaning.word !== selectedWord)) {
@@ -31,17 +36,6 @@ const WordList = () => {
           setMeaning(res.data[0]);
         });
     }
-  };
-
-  const onGroupSelect = (group) => {
-    setSelectedGroup(group);
-    setSelectedWord(null);
-    setMeaning(null);
-  };
-
-  const onWordSelect = (word) => {
-    setSelectedWord(word);
-    setMeaning(null);
   };
 
   const copyToClipboard = (word) => {
@@ -61,126 +55,131 @@ const WordList = () => {
     <div className='pt-5'>
       <p className='p-2 font-bold text-lg'>Groups</p>
       {words && (
-        <div className='flex justify-start flex-wrap gap-1 p-2'>
-          {Object.keys(words.words).map((group) => {
-            return (
-              <span key={group}>
-                <button
-                  className={
-                    'border-2 rounded-md border-black p-1 hover:bg-black hover:text-white' +
-                    (group === selectedGroup ? ' bg-black text-white' : '')
-                  }
-                  onClick={() => onGroupSelect(group)}
-                >
-                  {group.split('_').join(' ')}
-                </button>
-              </span>
-            );
-          })}
-        </div>
-      )}
-      {selectedGroup && <p className='p-2 font-bold text-lg'>Words</p>}
-      {selectedGroup && (
-        <div className='flex justify-start flex-wrap gap-1 p-2'>
-          {words.words[selectedGroup].map((word) => {
-            return (
-              <span key={word}>
-                <button
-                  className={
-                    'border-2 rounded-md border-black p-1 hover:bg-black hover:text-white' +
-                    (word === selectedWord ? ' bg-black text-white' : '')
-                  }
-                  onClick={() => onWordSelect(word)}
-                >
-                  {word}
-                </button>
-              </span>
-            );
-          })}
-        </div>
-      )}
-
-      {selectedWord && (
-        <div>
-          <div className='flex justify-center pt-10 pb-10 gap-2'>
-            <p className='text-5xl'>{selectedWord}</p>
+        <>
+          <div className='flex justify-start flex-wrap gap-1 p-2'>
+            {Object.keys(words.words).map((group) => {
+              return (
+                <span key={group}>
+                  <Link to={group}>
+                    <button
+                      className={
+                        'border-2 rounded-md border-black p-1 hover:bg-black hover:text-white' +
+                        (group === selectedGroup ? ' bg-black text-white' : '')
+                      }
+                    >
+                      {group.split('_').join(' ')}
+                    </button>
+                  </Link>
+                </span>
+              );
+            })}
           </div>
-          <div className='flex justify-center gap-2'>
-            <button
-              className='border-2 rounded-md border-black p-1 hover:bg-black hover:text-white'
-              onClick={() => copyToClipboard(selectedWord)}
-            >
-              copy
-            </button>
-            <button
-              className='border-2 rounded-md border-black p-1 hover:bg-black hover:text-white'
-              onClick={showMeaning}
-            >
-              show meaning
-            </button>
-            <button
-              className='border-2 rounded-md border-black p-1 hover:bg-black hover:text-white'
-              onClick={() => gotoMeaning(selectedWord)}
-            >
-              Google dictionary &#x2197;
-            </button>
-          </div>
-        </div>
-      )}
 
-      {meaning && (
-        <div className='pt-5'>
-          {meaning.meanings.map((m) => {
-            return (
-              <div className='p-2' key={selectedWord + m.partOfSpeech}>
-                <p className='italic'>{m.partOfSpeech}</p>
-                <div className='pl-4'>
-                  {m.definitions.map((d, index) => {
-                    return (
-                      <p key={d.definition}>
-                        {index + 1 + '. ' + d.definition}
-                      </p>
-                    );
-                  })}
-                </div>
-                {m.synonyms.length ? (
-                  <div className='pl-4 flex justify-start flex-wrap pt-2 gap-2'>
-                    <p className='font-bold'>synonyms: </p>
-                    {m.synonyms.map((s) => {
-                      return (
-                        <p
-                          key={s}
-                          className='border rounded-xl border-slate-500 px-1'
-                        >
-                          {s}
-                        </p>
-                      );
-                    })}
-                  </div>
-                ) : (
-                  <></>
-                )}
-                {m.antonyms.length ? (
-                  <div className='pl-4 flex justify-start flex-wrap pt-2 gap-2'>
-                    <p className='font-bold'>antonyms: </p>
-                    {m.antonyms.map((s) => {
-                      return (
-                        <p
-                          key={s}
-                          className='border rounded-xl border-slate-500 px-1'
-                        >
-                          {s}
-                        </p>
-                      );
-                    })}
-                  </div>
-                ) : (
-                  <></>
-                )}
+          {selectedGroup && <p className='p-2 font-bold text-lg'>Words</p>}
+          {selectedGroup && (
+            <div className='flex justify-start flex-wrap gap-1 p-2'>
+              {words.words[selectedGroup].map((word) => {
+                return (
+                  <span key={word}>
+                    <Link to={selectedGroup + '/' + word}>
+                      <button
+                        className={
+                          'border-2 rounded-md border-black p-1 hover:bg-black hover:text-white' +
+                          (word === selectedWord ? ' bg-black text-white' : '')
+                        }
+                      >
+                        {word}
+                      </button>
+                    </Link>
+                  </span>
+                );
+              })}
+            </div>
+          )}
+
+          {selectedWord && (
+            <div>
+              <div className='flex justify-center pt-10 pb-10 gap-2'>
+                <p className='text-5xl'>{selectedWord}</p>
               </div>
-            );
-          })}
-        </div>
+              <div className='flex justify-center gap-2'>
+                <button
+                  className='border-2 rounded-md border-black p-1 hover:bg-black hover:text-white'
+                  onClick={() => copyToClipboard(selectedWord)}
+                >
+                  copy
+                </button>
+                <button
+                  className='border-2 rounded-md border-black p-1 hover:bg-black hover:text-white'
+                  onClick={showMeaning}
+                >
+                  show meaning
+                </button>
+                <button
+                  className='border-2 rounded-md border-black p-1 hover:bg-black hover:text-white'
+                  onClick={() => gotoMeaning(selectedWord)}
+                >
+                  Google dictionary &#x2197;
+                </button>
+              </div>
+            </div>
+          )}
+
+          {meaning && (
+            <div className='pt-5'>
+              {meaning.meanings.map((m) => {
+                return (
+                  <div className='p-2' key={selectedWord + m.partOfSpeech}>
+                    <p className='italic font-bold'>{m.partOfSpeech}</p>
+                    <div className='pl-4'>
+                      {m.definitions.map((d, index) => {
+                        return (
+                          <p key={d.definition}>
+                            {index + 1 + '. ' + d.definition}
+                          </p>
+                        );
+                      })}
+                    </div>
+                    {m.synonyms.length ? (
+                      <div className='pl-4 flex justify-start flex-wrap pt-2 gap-2'>
+                        <p className=''>synonyms: </p>
+                        {m.synonyms.map((s) => {
+                          return (
+                            <p
+                              key={s}
+                              className='border rounded-xl border-slate-500 px-1'
+                            >
+                              {s}
+                            </p>
+                          );
+                        })}
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                    {m.antonyms.length ? (
+                      <div className='pl-4 flex justify-start flex-wrap pt-2 gap-2'>
+                        <p className=''>antonyms: </p>
+                        {m.antonyms.map((s) => {
+                          return (
+                            <p
+                              key={s}
+                              className='border rounded-xl border-slate-500 px-1'
+                            >
+                              {s}
+                            </p>
+                          );
+                        })}
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </>
       )}
     </div>
   );
