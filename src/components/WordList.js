f7af7e01520996c86dfc7579f43247ccc29d40b5
vistej <teja.pvt@gmail.com>
2022-06-26 19:18:02 +0530

    added expand button

--- src/components/WordList.js
+++ src/components/WordList.js
@@ -7,6 +7,7 @@ const WordList = () => {
   const [selectedGroup, setSelectedGroup] = React.useState(null);
   const [selectedWord, setSelectedWord] = React.useState(null);
   const [meaning, setMeaning] = React.useState(null);
+  const [isExpanded, setIsExpanded] = React.useState(true);
   const { group, word } = useParams();
   const navigate = useNavigate();
 
@@ -30,12 +31,12 @@ const WordList = () => {
   }, []);
 
   document.onkeydown = (e) => {
-    if (e.key === "ArrowLeft") {
-      onPrev()
-    } else if(e.key === "ArrowRight") {
-      onNext()
+    if (e.key === 'ArrowLeft') {
+      onPrev();
+    } else if (e.key === 'ArrowRight') {
+      onNext();
     }
-  }
+  };
 
   const showMeaning = () => {
     if (selectedWord && (!meaning || meaning.word !== selectedWord)) {
@@ -48,12 +49,15 @@ const WordList = () => {
   };
 
   const copyToClipboard = (word) => {
-    if(navigator.clipboard) {
-      navigator.clipboard.writeText(word).then(() => {
-        console.log('Copied to clipboard');
-      }).catch(err => {
-        console.log('Failed to copy to clipboard', err);
-      })
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(word)
+        .then(() => {
+          console.log('Copied to clipboard');
+        })
+        .catch((err) => {
+          console.log('Failed to copy to clipboard', err);
+        });
     }
   };
 
@@ -62,81 +66,101 @@ const WordList = () => {
   };
 
   const onPrev = () => {
-    const index = words.words[selectedGroup].findIndex((w) => w === selectedWord);
-    if (index-1 >= 0 && index-1 < words.words[selectedGroup].length) {
-      const newWord = words.words[selectedGroup][index-1];
-      navigate(`/${selectedGroup}/${newWord.trim()}`, {replace: true});
-    }
-  }
-
-  const onNext = () => {
-    const index = words.words[selectedGroup].findIndex((w) => w === selectedWord);
-    if (index+1 >= 0 && index+1 < words.words[selectedGroup].length) {
-      const newWord = words.words[selectedGroup][index+1];
+    const index = words.words[selectedGroup].findIndex(
+      (w) => w === selectedWord
+    );
+    if (index - 1 >= 0 && index - 1 < words.words[selectedGroup].length) {
+      const newWord = words.words[selectedGroup][index - 1];
       navigate(`/${selectedGroup}/${newWord.trim()}`, { replace: true });
     }
-  }
+  };
+
+  const onNext = () => {
+    const index = words.words[selectedGroup].findIndex(
+      (w) => w === selectedWord
+    );
+    if (index + 1 >= 0 && index + 1 < words.words[selectedGroup].length) {
+      const newWord = words.words[selectedGroup][index + 1];
+      navigate(`/${selectedGroup}/${newWord.trim()}`, { replace: true });
+    }
+  };
 
   return (
     <div className='pt-5'>
-      <p className='p-2 font-bold text-lg'>Groups</p>
       {words && (
         <>
-          <div className='flex justify-start flex-wrap gap-1 p-2'>
-            {Object.keys(words.words).map((group) => {
-              return (
-                <span key={group}>
-                  <Link to={group}>
-                    <button
-                      className={
-                        'border-2 rounded-md border-black p-1 hover:bg-black hover:text-white' +
-                        (group === selectedGroup ? ' bg-black text-white' : '')
-                      }
-                    >
-                      {group.split('_').join(' ')}
-                    </button>
-                  </Link>
-                </span>
-              );
-            })}
+          <div className='flex justify-between'>
+            <p className='p-2 font-bold text-lg'>Groups</p>
+            <button
+              className=''
+              onClick={() => setIsExpanded(!isExpanded)}
+            >
+              {isExpanded ? 'minimize ▴' : 'expand ▾'}
+            </button>
           </div>
-
-          {selectedGroup && <p className='p-2 font-bold text-lg'>Words</p>}
-          {selectedGroup && (
-            <div className='flex justify-start flex-wrap gap-1 p-2'>
-              {words.words[selectedGroup].map((word) => {
-                return (
-                  <span key={word}>
-                    <Link to={selectedGroup + '/' + word}>
-                      <button
-                        className={
-                          'border-2 rounded-md border-black p-1 hover:bg-black hover:text-white' +
-                          (word === selectedWord ? ' bg-black text-white' : '')
-                        }
-                      >
-                        {word}
-                      </button>
-                    </Link>
-                  </span>
-                );
-              })}
-            </div>
+          {isExpanded && (
+            <>
+              {' '}
+              <div className='flex justify-start flex-wrap gap-1 p-2'>
+                {Object.keys(words.words).map((group) => {
+                  return (
+                    <span key={group}>
+                      <Link to={group}>
+                        <button
+                          className={
+                            'border-2 rounded-md border-black p-1 hover:bg-black hover:text-white' +
+                            (group === selectedGroup
+                              ? ' bg-black text-white'
+                              : '')
+                          }
+                        >
+                          {group.split('_').join(' ')}
+                        </button>
+                      </Link>
+                    </span>
+                  );
+                })}
+              </div>
+              {selectedGroup && <p className='p-2 font-bold text-lg'>Words</p>}
+              {selectedGroup && (
+                <div className='flex justify-start flex-wrap gap-1 p-2'>
+                  {words.words[selectedGroup].map((word) => {
+                    return (
+                      <span key={word}>
+                        <Link to={selectedGroup + '/' + word}>
+                          <button
+                            className={
+                              'border-2 rounded-md border-black p-1 hover:bg-black hover:text-white' +
+                              (word === selectedWord
+                                ? ' bg-black text-white'
+                                : '')
+                            }
+                          >
+                            {word}
+                          </button>
+                        </Link>
+                      </span>
+                    );
+                  })}
+                </div>
+              )}
+            </>
           )}
           <hr />
           {selectedWord && (
             <div>
               <div className='flex justify-center gap-2 pt-5'>
-              <button
+                <button
                   className='border-2 rounded-md border-black p-1 hover:bg-black hover:text-white'
                   onClick={onPrev}
                 >
-                  &larr;prev
+                  ◂ prev
                 </button>
                 <button
                   className='border-2 rounded-md border-black p-1 hover:bg-black hover:text-white'
                   onClick={onNext}
                 >
-                  next&rarr;
+                  next ▸
                 </button>
               </div>
               <div className='flex justify-center pt-10 pb-10 gap-2'>
